f158a60ae6c074b7fd640685d98ad9f6886a105a
Surbhi Patel <surbhi.patel@mindstix.com>
2021-11-30 09:10:57 +0530

    Add Comment Schema

--- models/post.js
+++ models/post.js
@@ -9,7 +9,12 @@ const postSchema = new mongoose.Schema({
         //needs to refer to user schema
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    //include the array of ids of all comments in this post schema itself
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
 },{
     timestamps: true
 });
